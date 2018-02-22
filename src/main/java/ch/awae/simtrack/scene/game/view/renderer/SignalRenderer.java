9227e4b038308d78c0dcef1cf878f0f341c877e6
Andreas Wälchli <andreas.waelchli@me.com>
2018-02-22 22:53:46 +0100

    closes #47: signal state rendering

--- src/main/java/ch/awae/simtrack/scene/game/view/renderer/SignalRenderer.java
+++ src/main/java/ch/awae/simtrack/scene/game/view/renderer/SignalRenderer.java
@@ -1,11 +1,14 @@
 package ch.awae.simtrack.scene.game.view.renderer;
 
 import java.awt.Color;
+import java.util.HashMap;
 import java.util.Map.Entry;
 
 import ch.awae.simtrack.core.Graphics;
 import ch.awae.simtrack.scene.game.Game;
 import ch.awae.simtrack.scene.game.model.entity.Signal;
+import ch.awae.simtrack.scene.game.model.entity.Train;
+import ch.awae.simtrack.scene.game.model.position.TileCoordinate;
 import ch.awae.simtrack.scene.game.model.position.TileEdgeCoordinate;
 
 public class SignalRenderer implements Renderer {
@@ -26,7 +29,15 @@ public class SignalRenderer implements Renderer {
 				g.fillRect(35, 14, 12, 12);
 			else
 				g.fillOval(35, 14, 12, 12);
+			
 			g.setColor(Color.RED);
+			
+			HashMap<TileCoordinate, Train> reservations = view.getModel().getTileReservations();
+			Train before = reservations.get(position.tile);
+			Train after = reservations.get(position.getOppositeDirection().tile);
+			if (before != null && before.equals(after))
+				g.setColor(Color.GREEN);
+			
 			g.fillOval(37, 16, 8, 8);
 		}
 	}
