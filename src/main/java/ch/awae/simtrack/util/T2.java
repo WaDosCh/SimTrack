8e4e375d3841239c2f85694bdc3f49d165df9a8e
Andreas Wälchli <andreas.waelchli@me.com>
2018-02-23 20:30:27 +0100

    gave T2<A,B> an SVUID

--- src/main/java/ch/awae/simtrack/util/T2.java
+++ src/main/java/ch/awae/simtrack/util/T2.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import lombok.Data;
 
 public @Data final class T2<A, B> implements Serializable {
+	private static final long serialVersionUID = 4760032006729811691L;
 
 	public final A _1;
 	public final B _2;
