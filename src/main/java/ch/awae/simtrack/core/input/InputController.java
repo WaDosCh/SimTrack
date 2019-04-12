b6229a36e9d9005ada9cde2998f22d442e2fdc00
Julian Schelker <judos@gmx.ch>
2019-04-12 03:06:34 +0200

    cleanup inputController

--- src/main/java/ch/awae/simtrack/core/input/InputController.java
+++ src/main/java/ch/awae/simtrack/core/input/InputController.java
@@ -1,18 +1,21 @@
 package ch.awae.simtrack.core.input;
 
+import static ch.awae.simtrack.core.input.InputEvent.InputEventType.CHANGE;
+import static ch.awae.simtrack.core.input.InputEvent.InputEventType.PRESS;
+import static ch.awae.simtrack.core.input.InputEvent.InputEventType.RELEASE;
+
 import java.awt.Point;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import ch.judos.generic.data.HashMapList;
 import lombok.Getter;
 
 public class InputController {
@@ -23,75 +26,32 @@ public class InputController {
 	public static final int MOUSE_LEFT = 0x0001_0000;
 	public static final int MOUSE_CENTER = 0x0002_0000;
 	public static final int MOUSE_RIGHT = 0x0003_0000;
+	public static final int MOUSE_SCROLL = 0x0004_0000;
 
-	// MOUSE MOTION
-	private double scroll = 0;
-	private Point mousePosition = new Point(0, 0);
+	// EVENT PROCESSING
+	private List<InputEvent> queuedInputEvents;
 
-	// LISTENER REGISTRY
-	private HashMap<Integer, Binding> keycodeBindings = new HashMap<>();
-	private HashMap<InputActionI, Binding> actionBindings = new HashMap<>();
-	private HashMapList<Integer, Binding> bindings = new HashMapList<>();
+	// CACHE
+	private HashSet<Integer> holdKeyCodes;
+	private Point currentMousePosition;
 
-	// BINDING PROVIDERS
-	public synchronized Binding getBinding(int keycode) {
-		Binding binding = keycodeBindings.get(keycode);
-		if (binding == null) {
-			binding = new Binding();
-			keycodeBindings.put(keycode, binding);
-			bindings.put(keycode, binding);
-		}
-		return binding;
-	}
-
-	public Binding getBinding(InputActionI action) {
-		Binding binding = actionBindings.get(action);
-		if (binding == null) {
-			binding = new Binding();
-			actionBindings.put(action, binding);
-			for (int keycode : action.getKeyCodes()) {
-				bindings.put(keycode, binding);
-			}
-		}
-		return binding;
-	}
-
-	// MOUSE UTILITIES
-	/**
-	 * provides the current mouse scroll speed.
-	 * 
-	 * @return the current scroll speed.
-	 */
-	public double getScroll() {
-		double result = this.scroll;
-		this.scroll = 0;
-		return result;
-	}
-
-	public Point getMousePosition() {
-		return new Point(this.mousePosition);
-	}
-
-	// EVENT HANLDING
-	private void handle(int keycode, boolean state) {
-		ArrayList<Binding> list = bindings.getList(keycode);
-		if (list != null) {
-			for (Binding binding : list) {
-				binding.update(state);
-			}
-		}
+	public InputController() {
+		this.holdKeyCodes = new HashSet<>();
+		this.currentMousePosition = new Point(-1, -1);
 	}
 
 	// ADAPTERS
 	private @Getter KeyAdapter keyboard = new KeyAdapter() {
 		@Override
 		public synchronized void keyPressed(KeyEvent e) {
-			handle(e.getKeyCode(), true);
+			queuedInputEvents.add(new InputEvent(e.getKeyCode(), PRESS, 0, holdKeyCodes, currentMousePosition));
+			holdKeyCodes.add(e.getKeyCode());
 		}
 
 		@Override
 		public synchronized void keyReleased(KeyEvent e) {
-			handle(e.getKeyCode(), false);
+			holdKeyCodes.remove(e.getKeyCode());
+			queuedInputEvents.add(new InputEvent(e.getKeyCode(), RELEASE, 0, holdKeyCodes, currentMousePosition));
 		}
 	};
 
@@ -107,39 +67,40 @@ public class InputController {
 			Point p = e.getPoint();
 			if (p == null)
 				return;
-			InputController.this.mousePosition = p;
+			currentMousePosition = p;
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			this.setBtn(e.getButton(), true);
+			int code = btnToCode(e.getButton());
+			queuedInputEvents.add(new InputEvent(code, PRESS, 0, holdKeyCodes, currentMousePosition));
+			holdKeyCodes.add(code);
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
-			this.setBtn(e.getButton(), false);
+			int code = btnToCode(e.getButton());
+			holdKeyCodes.remove(code);
+			queuedInputEvents.add(new InputEvent(code, RELEASE, 0, holdKeyCodes, currentMousePosition));
 		}
 
 		@Override
 		public void mouseWheelMoved(MouseWheelEvent e) {
-			InputController.this.scroll = e.getPreciseWheelRotation();
+			System.out.println(e.getButton());
+			int code = btnToCode(e.getButton());
+			queuedInputEvents
+					.add(new InputEvent(code, CHANGE, e.getPreciseWheelRotation(), holdKeyCodes, currentMousePosition));
 		}
 
-		private void setBtn(int button, boolean value) {
-			switch (button) {
-				case MouseEvent.BUTTON1:
-					handle(MOUSE_LEFT, value);
-					break;
-				case MouseEvent.BUTTON2:
-					handle(MOUSE_CENTER, value);
-					break;
-				case MouseEvent.BUTTON3:
-					handle(MOUSE_RIGHT, value);
-					break;
-				default:
-					logger.info("Unknown mouse button: " + button);
-					break;
-			}
+		private int btnToCode(int button) {
+			if (button == MouseEvent.BUTTON1)
+				return MOUSE_LEFT;
+			if (button == MouseEvent.BUTTON2)
+				return MOUSE_CENTER;
+			if (button == MouseEvent.BUTTON3)
+				return MOUSE_RIGHT;
+			logger.warn("Unknown mouse button: " + button);
+			return MOUSE_CENTER;
 		}
 
 	};
