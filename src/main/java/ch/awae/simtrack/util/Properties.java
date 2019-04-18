ae0269a6d85e8f742db015ba9bbd8c17f395e4b6
Julian Schelker <judos@gmx.ch>
2019-04-18 13:16:10 +0200

    can now load colors with alpha value from property files

--- src/main/java/ch/awae/simtrack/util/Properties.java
+++ src/main/java/ch/awae/simtrack/util/Properties.java
@@ -18,6 +18,13 @@ public class Properties {
 
 	public Color getColor(String id) {
 		String v = getString(id);
+		// special case for colors with alpha value
+		if (v.length() == 9) {
+			int alpha = Integer.parseInt(v.substring(7), 16);
+			v = v.substring(0, 7);
+			Color c = Color.decode(v);
+			return new Color(c.getRed(), c.getGreen(), c.getBlue(), alpha);
+		}
 		return Color.decode(v);
 	}
 
